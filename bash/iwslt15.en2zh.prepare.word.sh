a6b8a141400b2bed523f0f003333e42bf3bcdbea
Epsilon Lee <epsilonlee.green@gmail.com>
2019-01-08 10:00:42 +0800

    ...

--- bash/iwslt15.en2zh.prepare.word.sh
+++ bash/iwslt15.en2zh.prepare.word.sh
@@ -1,11 +1,15 @@
-TEXT=examples/translation/iwslt15/en-zh/final
+# prepare train
+# TEXT=examples/translation/iwslt15/en-zh/final
+# prepare proto
+TEXT=examples/translation/iwslt15/en-zh/final/brown/final
 python preprocess.py \
     --source-lang en \
     --target-lang zh \
     --trainpref $TEXT/train \
     --validpref $TEXT/valid \
     --testpref $TEXT/test \
-    --nwordssrc 31098 \
-    --nwordstgt 39064 \
-    --destdir data-bin/iwslt15.tokenized.en-zh.word.raw \
-    --output-format raw
+    --nwordssrc 50 \
+    --nwordstgt 50 \
+    --destdir data-bin/iwslt15.tokenized.en-zh.word.proto \
+    --output-format raw \
+    --padding-factor 1
