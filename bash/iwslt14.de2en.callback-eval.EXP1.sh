f4b116c6f0119418aa547b2fae4981f9b72d7116
Epsilon Lee <epsilonlee.green@gmail.com>
2019-01-13 16:52:23 +0800

    callback valid scripts for EXP1 on iwslt.de2en

---
+++ bash/iwslt14.de2en.callback-eval.EXP1.sh
@@ -0,0 +1,5 @@
+python callback_validation.py \
+    --ckpt-path checkpoints/iwslt14.de-en.transformer.word.EXP1 \
+    --input-data data-bin/iwslt14.tokenized.de-en.nobpe.vocabSwitchout \
+    --EXP EXP1 \
+    --corpus iwslt14.de-en \
