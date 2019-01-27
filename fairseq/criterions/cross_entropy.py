d883d797cd1e197aa1a0c780fc58b4de42d7e2f1
Epsilon Lee <epsilonlee.green@gmail.com>
2019-01-27 14:45:56 +0800

    fix self.task.args to self.args

--- fairseq/criterions/cross_entropy.py
+++ fairseq/criterions/cross_entropy.py
@@ -40,7 +40,7 @@ class CrossEntropyCriterion(FairseqCriterion):
             'nsentences': sample['target'].size(0),
             'sample_size': sample_size,
         }
-        if self.task.args.repeat_batch > 1:
+        if self.args.repeat_batch > 1:
             loss *= self.task.args.repeat_batch
         return loss, sample_size, logging_output
 
